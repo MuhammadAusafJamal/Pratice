a1e4530c2b12ff67d0c163ff46c5894d73a3dcdf
Muhammad Ausaf Jamal <ausaffarooqui17@gmail.com>
2023-10-17 21:18:51 +0500

    Addes files via upload
    Basic page of React. Explored component and tried list rendering using .map().

---
+++ ReactJS-List-Rendering-Components/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import Ques1 from './components/question1';
+import Ques2 from './components/question2';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Ques1/>
+      <Ques2/>
+    </div>
+  );
+}
+
+export default App;
